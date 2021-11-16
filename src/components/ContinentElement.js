89999433e524ca31acb7746b75106afff88f16b8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-16 13:34:47 +0100

    Fetch data from the Api and display the list of African Continents

---
+++ src/components/ContinentElement.js
@@ -0,0 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchContinent } from '../redux/continent/Continent';
+
+const ContinentElement = () => {
+  const dispatch = useDispatch();
+  const { continentReducer } = useSelector((state) => state);
+  useEffect(() => {
+    dispatch(fetchContinent());
+  }, []);
+  return (
+    <>
+      {continentReducer.map(({ countryInfo: { _id: id, flag }, country }) => (
+        <Link to="/country" key={id}>
+          <div>
+            <span>{country}</span>
+            <img src={flag} alt={`${country}'s Flag`} />
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default ContinentElement;
