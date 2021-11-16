31c46e0a81287aa30628f15b134ab563d0c916d6
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-16 12:15:23 +0100

    Create variables actions, action creators and reducers which will be used in the project

---
+++ src/redux/continent/Continent.js
@@ -0,0 +1,24 @@
+const FETCH_CONTINENT = 'airpollution/continent/FETCH_CONTINENT';
+const APIURL = 'https://disease.sh/v3/covid-19/countries';
+const initialState = [];
+
+export const fetchContinent = () => async (dispatch) => {
+  const response = await fetch(APIURL);
+  const data = await response.json();
+  const continentData = data.filter((item) => item.countryInfo.continent === 'Africa');
+  dispatch({
+    type: FETCH_CONTINENT,
+    continentData,
+  });
+};
+
+const continentReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CONTINENT:
+      return action.continentData;
+    default:
+      return state;
+  }
+};
+
+export default continentReducer;
