1b9bc5c274809eadd0c631c3ca14095dbe06f245
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-03-25 15:13:05 +0100

    Make the WorldElement component the home page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import {
 } from 'react-router-dom';
 import ContinentElement from './components/ContinentElement';
 import CountryElement from './components/CountryElement';
+import WorldElement from './components/WorldElement';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
+          <WorldElement />
+        </Route>
+        <Route path="/continent/:continentName">
           <ContinentElement />
         </Route>
         <Route path="/country">
