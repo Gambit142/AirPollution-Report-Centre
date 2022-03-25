c0328339d697da01b9a65918ccdadc78d63f76be
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-03-25 15:18:24 +0100

    Create an array of continent images

--- src/redux/continent/Continent.js
+++ src/redux/continent/Continent.js
@@ -4,8 +4,7 @@ export const FETCH_CONTINENT = 'airpollution/continent/FETCH_CONTINENT';
 const initialState = [];
 
 export const fetchContinent = () => async (dispatch) => {
-  const data = await fetchContinentApi();
-  const continentData = data.filter((item) => item.continent === 'Africa');
+  const continentData = await fetchContinentApi();
   dispatch({
     type: FETCH_CONTINENT,
     continentData,
